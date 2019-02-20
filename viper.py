0143e11906153e798823ca191cc2cc5cfb9ff979
MandarinDuck <duck@duck.local>
2019-02-20 15:49:49 +0000

    v0.1.9

--- viper.py
+++ viper.py
@@ -23,7 +23,7 @@ viper_dag = DAG(
     start_date= datetime.now(),
     schedule_interval= None,
     #start_date= datetime(2019, 1, 30),
-    #schedule_interval= '0 2 * * *' #timedelta(days= 1),
+    #schedule_interval= '0 2 * * *',
     catchup= False
 )
 
@@ -31,7 +31,7 @@ viper_dag = DAG(
 def assign_task_to_dag(target_dag):
 
     repo_name = "airflow-viper"
-    repo_release_tag = "v0.1.8"
+    repo_release_tag = "v0.1.9"
     VIPER_IMAGE = (
         f"593291632749.dkr.ecr.eu-west-1.amazonaws.com/{repo_name}:{repo_release_tag}"
     )
@@ -45,6 +45,8 @@ def assign_task_to_dag(target_dag):
             "DATABASE": "anvil_beta",
             "OUTPUT_LOC": "alpha-anvil/curated",
             "AWS_DEFAULT_REGION": "eu-west-1",
+            "ATHENA_BUCKET": "alpha-nomis-discovery",
+            "ATHENA_FOLDER": "__viper_tmp__",
         },
         labels={"viper": viper_dag.dag_id},
         name="viper",
