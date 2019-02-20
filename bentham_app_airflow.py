db55aafdfa0afc3f1aff0d72f36c0df1ba7bc48a
joeprinold <35369456+joeprinold@users.noreply.github.com>
2019-02-20 12:36:22 +0000

    Add AWS_DEFAULT_REGION specifications

--- bentham_app_airflow.py
+++ bentham_app_airflow.py
@@ -6,7 +6,7 @@ from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOpera
 from airflow.utils.dates import days_ago
 
 # Define your docker image and the AWS role that will run the image (based on your airflow-repo)
-IMAGE = "593291632749.dkr.ecr.eu-west-1.amazonaws.com/airflow-bentham-app:v0.2.5"
+IMAGE = "593291632749.dkr.ecr.eu-west-1.amazonaws.com/airflow-bentham-app:v0.2.6"
 ROLE = "airflow_bentham_app"
 
 # Task arguments
@@ -21,17 +21,14 @@ task_args = {
 }
 
 # # # Define your DAG
-# Some notes:
-# setting - (start_date=datetime.now() and schedule_interval=None) is a way to set up you tag so it can only be triggered manually
-# To actually put it on a schedule you can set something like:
-# start_date=datetime(2018, 8, 1), schedule_interval=timedelta(days=1)
 dag = DAG(
     "bentham_app_2",
     default_args=task_args,
     description="Check s3 for new phone data, then add to database if present.",
     start_date=datetime(2019, 1, 6, 2),
     schedule_interval='0 2 * * *',
-    catchup=False
+    catchup=False,
+    env_vars={"AWS_DEFAULT_REGION": "eu-west-1"}
 )
 
 task_id = "bentham-app-data-update"
